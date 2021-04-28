32235dde7cb6c980a6b628753cced82656a4c0aa
Alexey <lskalkin1@mail.ru>
2021-04-28 10:34:56 +0300

    Again

---
+++ lesson5/lesson5.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hello, this is my first programm. ";
+	cout << "Enter two value:";
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
