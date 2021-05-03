0c00d37539c2402656b8509d02b4a01f750bdb78
Alexey <lskalkin1@mail.ru>
2021-05-03 09:48:11 +0300

    Add new lesson 21 and lesson 22

---
+++ lesson21/lesson21.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	cout << (((n / 1000) + 2) % ((n % 100 % 10) + 2) + ((n / 100 % 10) + 2) % ((n % 100 / 10)+ 2)) + 1;
+	return 0;
+}
---
+++ lesson22/lesson22.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int h, a, b;
+    cin >> h >> a >> b;
+    cout << ((h - b) + (a - b) - 1) / (a - b);
+    return 0;
+}
